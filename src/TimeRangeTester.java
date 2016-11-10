48e115098dc33501c35a5aa8746ff012daca7f1b
Kearns <akearns@igskgacgltsw001.gs.doi.net>
2016-11-10 16:04:01 -0700

    More TimeRange unit tests

--- src/TimeRangeTester.java
+++ src/TimeRangeTester.java
@@ -22,14 +22,31 @@ public class TimeRangeTester {
   TimeRange tr2 = new TimeRange(st2,st3);
   TimeRange tr3 = new TimeRange(st2,st4);
   TimeRange tr4 = new TimeRange(st1,st3);
+  TimeRange tr5 = new TimeRange(st1,st3);
+  
   
   @Test
   public void testOrdering(){
-    assertTrue((tr1.compareTo(tr2) > 0));
+    // tr2 is shorter than tr1 by 1
+    assertTrue(tr1.compareTo(tr2) > 0);
+  }
+  
+  @Test
+  public void testOrderEarlier(){
+    // same length, but tr1 is earlier
+    assertTrue(tr1.compareTo(tr3) < 0);
+  }
+  
+  @Test
+  public void testOrderEquals(){
+    // same length, same times, means equals
+    assertTrue(tr4.compareTo(tr5) == 0);
   }
   
   @Test
   public void testMerge(){
+    // tr4 is from st1 to st3, and tr3 from st2 to st4
+    // result range should be from st2 to st3 (== tr2)
     TimeRange testTime = new TimeRange(tr4,tr3);
     assertEquals(testTime,tr2);  
   }
