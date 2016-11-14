93ca7afb9a384a8ff3db01b4f0199e8cf94c0228
Kearns <akearns@igskgacgltsw001.gs.doi.net>
2016-11-14 11:02:29 -0700

    added notes to incompletable unit tests

--- src/asl/azimuth/test/SeedSplitterInputTest.java
+++ src/asl/azimuth/test/SeedSplitterInputTest.java
@@ -16,8 +16,18 @@ public class SeedSplitterInputTest {
 
   // This module is used to make sure file time ranges match
   // since this is an assumption relied upon by our code
+  // TODO:
+  // What we WANT this to do is to read in DataSets and ContiguousBlocks
+  // from the result of ChannelSelector or other input generator
+  // then we test to see if their time scales match by index
+  // which is a vital assumption in how FilterBlocks are currently constructed
+  // HOWEVER, we can't do that without a decent non-GUI tool for doing this,
+  // but right now the main access points to that data are code like
+  // the ChannelSelector class, which has it strongly coupled with the interface
+  // making writing tests like this difficult -- if not impossible -- without
+  // extensive re-writes of the code
   
-  String fname = "00_LH1.512.SEED"; // file to test on
+  String fname = "samples/00_LH1.512.SEED"; // file to test on
   File[] file;
   List<DataSet> dataSets;
   List<ContiguousBlock> contigBlocks;
